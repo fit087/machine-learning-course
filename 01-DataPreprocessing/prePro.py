e76359d4ed274bd0148fa79a822e5c9e3aab4c60
Adolfo Correa <adolfo.correa@coc.ufrj.br>
2020-01-29 17:34:28 -0300

    feat:treat missing values with SimpleImputer

--- 01-DataPreprocessing/prePro.py
+++ 01-DataPreprocessing/prePro.py
@@ -8,6 +8,9 @@ Este é um arquivo de script temporário.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+
+os.chdir('/home/acorrea/Dropbox/2020.1/ML/Machine Learning A-Z/Part 1 - Data Preprocessing')
 
 # Read a dataset from a csv file
 data = pd.read_csv('Data.csv')
@@ -16,6 +19,18 @@ data = pd.read_csv('Data.csv')
 X = data.iloc[:,:-1].values
 Y = data.iloc[:,-1].values
 
-print(data)
-print (X)
-print (Y)
+#print(data)
+#print (X)
+#print (Y)
+
+# Taking care of missing data
+#from sklearn.preprocessing import Imputer
+#imputer = Imputer(missing_values=np.nan, strategy="mean", axis=0)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+print(X)
+
